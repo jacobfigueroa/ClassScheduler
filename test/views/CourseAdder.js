f91c16178f96320e42230a98a7cd1823c4567d08
Jacob Figueroa <jacobx04@hotmail.com>
2015-02-03 10:18:42 -0600

    User can now add additional classes
    Cannot submit them yet

--- test/views/CourseAdder.js
+++ test/views/CourseAdder.js
@@ -33,21 +33,32 @@ $("#courseSelector").on("change","select", function(){
 			'url' : 'handlers/getCourses.php',
 			'data' : { 'subject' : subject} }
 			).done( function( data ) {
-				$('#courseList').empty();
+				var courseList = "#courseList" + idNumber
+				$(courseList).empty();
 				var classes = $.parseJSON(data);
 
 				var classList = $("<select>") //Instatiate a select
-				classList.attr("id","classListSelect"+courseCount)
+				classList.attr("id","classListSelect"+idNumber)
 				for(var i = 0; i < classes.length; i++)
 				{
 					var newOption = $("<option>")
 					newOption.attr("value", classes[i].CourseNumber).text(classes[i].CourseNumber + " " + classes[i].Title)
 					classList.append(newOption)
 				}
-				
-				$('#courseList').append(classList);
+
+				$(courseList).append(classList);
 			});
 	}
+});
+
+$("#addClass").click(function() {
+	courseCount += 1
+	var newCourse = $("#course0").clone()
+	newCourse.attr("id","course"+courseCount)
+	newCourse.children("#courseList0").attr("id","courseList"+courseCount)
+	newCourse.children("#classListSelect0").attr("id","classListSelect"+courseCount)
+	newCourse.children("#subjectListSelect0").attr("id","subjectListSelect"+courseCount)
+	$("#courseSelector").append(newCourse)
 });
 
 //$(staticAncestors).on(eventName, dynamicChild, function() {});
